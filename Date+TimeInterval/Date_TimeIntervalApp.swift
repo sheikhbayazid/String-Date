cf7971e2f21e43ebd7dae8f11634e082e2ab98a6
Sheikh Bayazid <sheikh.bayazid@shortcut.no>
2021-08-19 11:08:54 +0600

    Initial Commit

---
+++ Date+TimeInterval/Date_TimeIntervalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Date_TimeIntervalApp.swift
+//  Date+TimeInterval
+//
+//  Created by Sheikh Bayazid on 19/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Date_TimeIntervalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
